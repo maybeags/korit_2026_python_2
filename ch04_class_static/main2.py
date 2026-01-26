9287dd52efda80ce8fa45ddd0861b2f041892fbf
maybeags <suren0709@gmail.com>
2026-01-26 20:30:27 +0900

    feat: list-dict solution

--- ch04_class_static/main2.py
+++ ch04_class_static/main2.py
@@ -39,7 +39,7 @@ print(Bag.count)
 
 클래스 변수 / 클래스 메서드 활용 예제
 지시 사항
-1. Shop 클래스는 다음과 같은 변수를 가지고 있다
+1. Shop 클래스는 다음과 같은 클래스 변수를 가지고 있다
     total : 가게 전체 매출액
     menu_list : {메뉴명:가격}으로 이루어진 딕셔너리 요소를 가지는 리스트
     
@@ -53,3 +53,38 @@ Shop.sales("튀김", 5)        # 튀김을(를) 5개 판매
 3. 모든 매출을 작성한 뒤 다음과 같은 방식으로 전체 매출액을 확인합니다.
 print(f"매출 : {Shop.get_total()} 원")
 '''
+class Shop:
+    # 지시 사항에서 어떤 부분이 클래스 변수이고, 어떤 부분이 클래스 메서드인지,
+# 그리고 걔네가 어떤 방식으로 호출되는지, 결과값은 어떻게 되는지를 지시 사항을 읽고 감을 잡아보도록하겠습니다.
+    # 클래스 변수들
+    total = 0
+    menu_list = [{"떡볶이": 3000}, {"순대": 4000}, {"튀김": 500}, {"김밥": 2000}]
+    # 전체적으로는 list인데 내부 element는 key가 메뉴명이고 value가 가격인 dict
+
+    # 클래스 메서드
+    # print(f"매출 : {Shop.get_total()} 원")       # return이 존재한다
+    @classmethod
+    def get_total(cls):
+        return cls.total # 뭐라고 쓸지 클래스 변수 참조하는 법 아셔야 합니다.
+
+    @classmethod
+    def sales(cls, menu_name,  quantity):
+        # menu_list를 기준으로 list의 내부 element를 확인하는 방법이 뭐였는지 : 반복문
+        for menu_dict in cls.menu_list: # 메뉴 리스트이 내부는 dictionary들이니까
+            # dictionary 반복문 돌리면 뭐가 튀어나왔었나요?
+            # dictionary의 value를 확인하는 방법이 무엇이었는지 떠올리셔야 합니다.
+            if menu_name in menu_dict:      # menu_dict의 key 중에 menu_name과 겹치는게 있다면
+                cls.total += menu_dict[menu_name] * quantity
+                print(f'{menu_name}을(를) {quantity}개 판매')
+
+
+
+print(f"매출 : {Shop.get_total()} 원")
+Shop.sales("떡볶이", 1)       # 떡볶이을(를) 1개 판매
+Shop.sales("김밥", 2)        # 김밥을(를) 2개 판매
+Shop.sales("튀김", 5)        # 튀김을(를) 5개 판매
+print(f"매출 : {Shop.get_total()} 원")
+
+dict_example = {"name" : "김영", "age": 20, "address": "부산광역시 부산진구"}
+for key in dict_example:
+    print(key)
