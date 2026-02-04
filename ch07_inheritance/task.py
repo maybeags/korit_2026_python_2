c24094286cb5ee2103b9effba960faa48ac90a32
maybeags <suren0709@gmail.com>
2026-02-04 21:41:00 +0900

    feat: 20260204 python # 2 lesson ended

--- ch07_inheritance/task.py
+++ ch07_inheritance/task.py
@@ -99,13 +99,60 @@ rectangle.draw()
 print(f'직사각형의 넓이: {rectangle.area()}')
 
 실행 예
-반지름이 5인 원1이 생성되었습니다.
-이름이 원1인 원의 넓이는 ____ 입니다.
-원의 넓이 : ____
+반지름이 5인 원1이 생성되었습니다.                    # 이건 아무리 봐도 생성자 부분인거같고
+이름이 원1인 원의 넓이는 ____ 입니다.                # 얘가 그러면 draw()여야 할 것 같습니다.
+원의 넓이 : ____                                                     # 얘가 area() 메서드일 듯
 너비가 10, 높이가 8인 직사각형1이 생성되었습니다.
 이름이 직사각형1인 직사각형의 넓이는 ____ 입니다.
 직사각형의 넓이 : ____
 '''
+class Shape:
+    def __init__(self, name):
+        self.name = name
+
+    def draw(self):
+        print(f'{self.name}')
+
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, name, radius):
+        super().__init__(name)
+        self.radius = radius
+        print(f'반지름이 {self.radius}인 {self.name}이 생성되었습니다.')
+
+    def draw(self):
+        #super().draw()      # 부모 클래스의 메서드 이름만 가져오고 호출 안해도 뭐 그만입니다.
+        print(f'이름이 {self.name}인 원의 넓이는 {self.area()}입니다.')
+
+    def area(self):
+        # 부모 클래스의 area는 아무 로직이 없지만 여기서 정의하는 것도 가능합니다.
+        return 3.14*(self.radius**2)
+
+class Rectangle(Shape):
+    #생성자를 위에를 보고 정의하시면 되겠습니다.
+    def __init__(self, name, width, height):
+        super().__init__(name)
+        self.width = width
+        self.height = height
+        print(f'너비가 {self.width}, 높이가 {self.height}인 {self.name}이 생성되었습니다.')
+
+    def area(self):
+        return self.width*self.height
+
+    def draw(self):
+        print(f'이름이 {self.name}인 직사각형의 넓이는 {self.area()}입니다.')
+
+circle = Circle('원1', 5)
+circle.draw()
+print(f'원의 넓이 : {circle.area()}')
+print()
+rectangle = Rectangle('직사각형1', 10, 8)
+rectangle.draw()
+print(f'직사각형의 넓이: {rectangle.area()}')
+
+
 
 
 
